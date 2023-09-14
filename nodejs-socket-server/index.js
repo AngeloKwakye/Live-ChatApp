a0bfae851efe6f2c546b6cf0179c1d9bdeaca5d6
D'Angelo <dangelokwakye@gmail.com>
2023-09-14 16:00:10 +0000

    update index.js

--- nodejs-socket-server/index.js
+++ nodejs-socket-server/index.js
@@ -2,33 +2,32 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
-const {Server} = require("socket.io");
+const Server = require("socket.io");
 /* Using this commented code will give us CORS error */
 // const io = new Server(server);
-const io = new Server(server, {
+const io = Server(server, {
     cors: {
         origin: "*",
         methods: ["GET", "POST"]
     }
-})
-
-
-app.get('/',( req, res)=>{
-    res.send('<h1>Hello World</h1>')
-    res.send("hello world")
 });
 
-io.on('connected', (socket)=>{
+app.get('/',( req, res)=>{
+    console.log("Sever is running")
+    res.send('<h1>Server is Running!</h1>');
+});
+
+io.on('connection', (socket)=>{
     console.log("a user connected");
     socket.on('message', (msg)=>{
-        console.log("message:"+ msg)
-        io.emit("message", msg)
-    })
+        // console.log("message: "+ msg);
+        io.emit("message", msg);
+    });
 
     socket.on("disconnect",()=>{
-        console.log("user disconnected")
-    })
-})
+        console.log("user disconnected");
+    });
+});
 
 server.listen(3000,()=>{
     console.log('listening on *:3000');
